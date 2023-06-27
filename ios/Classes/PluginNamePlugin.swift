0d1eaefd41089bab491c14d6405f0764c4d14f76
JoseFrancisco93 <jfvc.93@gmail.com>
2023-06-27 22:45:18 +0000

    update

--- ios/Classes/PluginNamePlugin.swift
+++ ios/Classes/PluginNamePlugin.swift
@@ -1,36 +1,56 @@
-import Flutter
-import UIKit
-
 import UIKit
 import Flutter
+import UserNotifications
 
 public class PluginNamePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "plugin_name", binaryMessenger: registrar.messenger())
-    let instance = PluginNamePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if call.method == "showAlert" {
-      if let arguments = call.arguments as? [String: Any],
-         let message = arguments["message"] as? String {
-        showAlert(message: message)
-        result(nil)
-      } else {
-        result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
-      }
-    } else {
-      result(FlutterMethodNotImplemented)
-    }
-  }
-
-  private func showAlert(message: String) {
-    let alertController = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
-    alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    private var alertController: UIAlertController?
     
-    if let viewController = UIApplication.shared.keyWindow?.rootViewController {
-      viewController.present(alertController, animated: true, completion: nil)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "plugin_name", binaryMessenger: registrar.messenger())
+        let instance = PluginNamePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+      if call.method == "getPlatformVersion" {
+          result("iOS " + UIDevice.current.systemVersion)
+      } else if call.method == "showAlert" {
+          if let arguments = call.arguments as? [String: Any], let userName = arguments["userName"] as? String {
+              DispatchQueue.main.async {
+                  self.showSystemWindow(withUserName: userName)
+              }
+          }
+      } else if call.method == "closeAlert" {
+          DispatchQueue.main.async {
+              self.closeSystemWindow()
+          }
+      }
+}
+
+    private func showSystemWindow(withUserName userName: String) {
+        guard alertController == nil else {
+            return
+        }
+
+        let alert = UIAlertController(title: nil, message: "Llamada con \(userName)", preferredStyle: .alert)
+        alertController = alert
+
+        // Apariencia
+        let windowSubview = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        windowSubview.rootViewController = viewController
+        windowSubview.windowLevel = UIWindow.Level.alert + 1
+        windowSubview.isHidden = false
+
+        viewController.present(alert, animated: true, completion: nil)
+    }
+
+    private func closeSystemWindow() {
+        guard let alert = alertController else {
+            return
+        }
+
+        alertController = nil
+        alert.dismiss(animated: true, completion: nil)
+    }
 }
